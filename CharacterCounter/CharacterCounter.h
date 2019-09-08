b6b530278c00e6a3a6834c6e27f3e7ad697afaa6
Javal Nanda <javalnanda@gmail.com>
2019-09-08 15:49:51 +0530

    Initial Commit

---
+++ CharacterCounter/CharacterCounter.h
@@ -0,0 +1,19 @@
+//
+//  CharacterCounter.h
+//  CharacterCounter
+//
+//  Created by Javal Nanda on 08/09/19.
+//  Copyright © 2019 Javal Nanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CharacterCounter.
+FOUNDATION_EXPORT double CharacterCounterVersionNumber;
+
+//! Project version string for CharacterCounter.
+FOUNDATION_EXPORT const unsigned char CharacterCounterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CharacterCounter/PublicHeader.h>
+
+
